7b337b4465dbee394477647af0de32337ada57b9
sugino <sugino@kiroru-sugino.local>
2018-07-23 20:12:39 +0900

    injectorの定義

--- app/src/main/java/com/example/sugino/dagger_mvvm/base/BaseViewModel.kt
+++ app/src/main/java/com/example/sugino/dagger_mvvm/base/BaseViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.sugino.dagger_mvvm.base
 
 import android.arch.lifecycle.ViewModel
+import com.example.sugino.dagger_mvvm.injection.component.DaggerViewModelInjector
+import com.example.sugino.dagger_mvvm.injection.component.ViewModelInjector
+import com.example.sugino.dagger_mvvm.injection.module.NetWorkModule
+import com.example.sugino.dagger_mvvm.ui.post.PostListViewModel
 
 abstract class BaseViewModel: ViewModel() {
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+            .builder()
+            .networkModule(NetWorkModule)
+            .build()
 
+    init {
+        inject()
+    }
+
+    private fun inject() {
+        when (this) {
+            is PostListViewModel -> injector.inject(this)
+        }
+    }
 }
