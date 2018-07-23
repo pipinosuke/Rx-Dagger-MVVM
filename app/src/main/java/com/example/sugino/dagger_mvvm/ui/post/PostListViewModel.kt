28501e459eefe429cd04af13617816fae687a1d5
sugino <sugino@kiroru-sugino.local>
2018-07-23 20:36:00 +0900

    rxを入れた

--- app/src/main/java/com/example/sugino/dagger_mvvm/ui/post/PostListViewModel.kt
+++ app/src/main/java/com/example/sugino/dagger_mvvm/ui/post/PostListViewModel.kt
@@ -1,10 +1,53 @@
 package com.example.sugino.dagger_mvvm.ui.post
 
+import android.telephony.euicc.DownloadableSubscription
 import com.example.sugino.dagger_mvvm.base.BaseViewModel
 import com.example.sugino.dagger_mvvm.network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class PostListViewModel: BaseViewModel() {
     @Inject
     lateinit var postApi: PostApi
+
+    private lateinit var subscription: Disposable
+
+    init {
+        loadPosts()
+    }
+
+    private fun loadPosts() {
+        subscription = postApi.getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { onRetrivePostListStart() }
+                .doOnTerminate { onRetrivePostListFinish() }
+                .subscribe(
+                        { onRetrivePostListSuccess() },
+                        { onRetrivePostListError() }
+                )
+    }
+
+    private fun onRetrivePostListStart() {
+
+    }
+
+    private fun onRetrivePostListFinish() {
+
+    }
+
+    private fun onRetrivePostListSuccess() {
+
+    }
+
+    private fun onRetrivePostListError() {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
 }
