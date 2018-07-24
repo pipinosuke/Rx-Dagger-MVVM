5420b8ef502d2b71122481345d86b34fb6797b5b
sugino <sugino@kiroru-sugino.local>
2018-07-24 13:35:58 +0900

    query

--- app/src/main/java/com/example/sugino/dagger_mvvm/network/PostApi.kt
+++ app/src/main/java/com/example/sugino/dagger_mvvm/network/PostApi.kt
@@ -3,8 +3,12 @@ package com.example.sugino.dagger_mvvm.network
 import com.example.sugino.dagger_mvvm.model.Post
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PostApi {
     @GET("/posts")
-fun getPosts(): Observable<List<Post>>
+//fun getPosts(@Query("query") query: String? = null,
+//             @Query("id") id: Int = 1): Observable<List<Post>>
+    fun getPosts(@Query("query") query: String? = null,
+                 @Query("id") id: Int = 1): Observable<List<Post>>
 }
