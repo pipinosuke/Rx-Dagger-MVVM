3ef766e71093b95a23591ccb6449110d71e3dcb8
sugino <sugino@kiroru-sugino.local>
2018-07-26 16:48:07 +0900

    修正

--- app/src/main/java/com/example/sugino/dagger_mvvm/network/PostApi.kt
+++ app/src/main/java/com/example/sugino/dagger_mvvm/network/PostApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PostApi {
-    @GET("items?page=1&per_page=1")
+    @GET("items")
 //fun getPosts(@Query("query") query: String? = null,
 //             @Query("id") id: Int = 1): Observable<List<Post>>
     fun getPosts(@Query("query") query: String? = null,
